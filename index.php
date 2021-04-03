76530389d0c7ceb30b72766d58444f0f29e45512
Arkadiy Dmytryk <arkadrose@gmail.com>
2021-04-03 12:19:55 +0300

    add new command

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 const TOKEN = '1631861651:AAF8cxTry8RoZhB2nIFeLIXO6S5KzesuGJg';
 const BASE_URL = "https://api.telegram.org/bot".TOKEN."/";
 $link = pg_connect("dbname=d84pot3p9gld95 host=ec2-54-247-158-179.eu-west-1.compute.amazonaws.com port=5432 user=exkbqcvcpnvduz password=b2210c4a962c2b5d3826407c668631eac689a24dc9560d82138aeb38051a5b78 sslmode=require");
+$month = date("F");
 
 
 function sendRequest($method, $params = []) {
@@ -25,7 +26,7 @@ if($text == '/start') {
     sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
 
 }elseif($text == '/setinfo') {
-	$message = 'Type information this way: name, budget, month. Example => Bob, 1000, March';	
+	$message = 'Type information this way: name, budget. Example => Bob, 1000';	
     sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);	
 
 }else { // if user sending his name, budget and month
@@ -46,4 +47,28 @@ if($text == '/start') {
 	}
 }
 
+if($text == '/addcosts') {
+	$message = 'Type your data this way for a more secure: name, how you spent your money, how much did you spend it.
+	Example => Arkadiy, clothes, 100';
+	sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);	
+}
+
+$info = $explode(', ', $text);
+//if user sends some costs
+if(substr($text, 0, 1) != '/' && is_string($info[0]) && is_string($info[1]) && is_numeric($info[2])) { 
+	$name = $info[0];
+	$costs = $info[1];
+	$money = $info[2];
+	$query = pg_query($link, "ALTER TABLE {$name} ADD COLUMN {$costs} WHERE month = {$month};");
+	if(true) {
+		$query = pg_query($link, "INSERT INTO {$name} ({$costs}) VALUES ({$money});");
+	}else {
+		$message = 'Try again!';	
+        sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
+	}
+	
+}else {
+	$message = 'Try again!';	
+    sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
+}
 ?>
