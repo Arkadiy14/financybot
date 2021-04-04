ec75d63d05912ba286bad131c379bbadd058cff1
Arkadiy Dmytryk <arkadrose@gmail.com>
2021-04-04 16:18:05 +0300

    make it better

--- index.php
+++ index.php
@@ -59,16 +59,12 @@ if(!empty(is_string($info[0])) && !empty(is_numeric($info[1]))) { // if user is 
 	$costs = $info[1];
 	$money = $info[2];
 	$query = pg_query($link, "ALTER TABLE {$name} ADD COLUMN {$costs} INTEGER NOT NULL DEFAULT({$money});");
-	$result_query = pg_query($link, "SELECT * FROM {$name};");
-	$result = pg_fetch_result($result_query, 0, 0);
-
-	    if(!empty($result)) {
+	    if(true) {
 		    $message = 'Your costs were added!';	
             sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
         }else {
         	$message = 'Try again!';
-        	sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
-        }
+        } 
 
 	}else {
 		$message = 'Try again!';	
