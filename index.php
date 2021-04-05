eae5ca7a7f99d2872fe0002ff71d4959366bcb69
Arkadiy Dmytryk <arkadrose@gmail.com>
2021-04-05 11:22:29 +0300

    add new functions

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ if(!empty(is_string($info[0])) && !empty(is_numeric($info[1]))) { // if user is 
         	$query = pg_query($link, "ALTER TABLE {$name} ADD COLUMN {$costs} INTEGER NOT NULL DEFAULT({$money});");
         	$query2 = pg_query($link, "UPDATE {$name} SET remainder = {$new_remainder};");
         	$message = 'Your costs were added!';	
-        	sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $remainder);
+        	sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $remainder]);
         } 
 
 }else {
