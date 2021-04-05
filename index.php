e2247817bf6c5d5ad503cca4d37cd95ca9aa40e4
Arkadiy Dmytryk <arkadrose@gmail.com>
2021-04-05 12:33:14 +0300

    add new command

--- index.php
+++ index.php
@@ -33,6 +33,10 @@ if($text == '/start') {
 	$message = 'Type your data this way for a more secure: name, how you spent your money, how much did you spend it.
 	Example => bob, clothes, 100';
 	sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
+
+}elseif($text == '/getdata') {
+	$message = 'What do you want to know about? Type it this way: bob, food.';
+	sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
 }	
 
 
@@ -45,46 +49,62 @@ if(!empty(is_string($info[0])) && !empty(is_numeric($info[1]))) { // if user is 
     $budget = $info[1];
     $query = pg_query($link, "CREATE TABLE {$name} (budget INTEGER, remainder INTEGER, month VARCHAR (15) NOT NULL);");
     
-    if($query) {
+    if($query) { // if there is no table with name '$name'
 		$query = pg_query($link, "INSERT INTO {$name} (budget, remainder, month) VALUES ('{$budget}', '{$budget}', '{$month}');");
 		$message = 'You can use command /addcosts to add some costs.';
 		sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);	
-	}else {
+	}else { // if there is table with name '$name'
 		$message = 'Try again!';	
         sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
 	}
 
-}elseif(!empty(is_string($info[0])) && !empty(is_string($info[1])) && !empty(is_numeric($info[2]))) {//if user sends costs
+}elseif(!empty(is_string($info[0])) && !empty(is_string($info[1])) && !empty(is_numeric($info[2]))) {
+    // if user is sending some costs
 	$name = $info[0];
 	$costs = $info[1];
 	$money = $info[2];
-	$result = pg_query($link, "SELECT remainder FROM {$name};");
+	$result = pg_query($link, "SELECT remainder FROM {$name};"); // getting remainder to update it later 
 
-	    if(!$result) {
+	    if(!$result) { // if there is no remainder (no table) with name '$name'
 	    	$message = 'Try again!';	
             sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
 
-        }else {
+        }else { // if there is remainder (table) with name '$name'
         	$remainder_query = pg_query($link, "SELECT remainder FROM {$name} WHERE month = '{$month}';");
         	$remainder = pg_fetch_result($remainder_query, 0, 0);
-        	$new_remainder = $remainder - $money;
+        	$new_remainder = $remainder - $money; // update remainder 
 
-        	$costs_query = pg_query($link, "SELECT {$costs} FROM {$name} WHERE month = '{$month}';");
-        	if(!$costs_query) {
+        	$costs_query = pg_query($link, "SELECT {$costs} FROM {$name} WHERE month = '{$month}';"); 
+        	// checking if there already are some costs '$costs'
+        	if(!$costs_query) { // if there aren't
                 $query = pg_query($link, "ALTER TABLE {$name} ADD COLUMN {$costs} INTEGER NOT NULL DEFAULT({$money});");
-        	}else {
+        	}else { // if there are
         		$costs_result = pg_fetch_result($costs_query, 0, 0);
-        		$new_money = $costs_result + $money;
+        		$new_money = $costs_result + $money; // adding new amount of money to costs '$costs'
         	  $query = pg_query($link, "UPDATE {$name} SET {$costs} = {$new_money};");
         	}
 
-        	$query2 = pg_query($link, "UPDATE {$name} SET remainder = {$new_remainder};");
+        	$query2 = pg_query($link, "UPDATE {$name} SET remainder = {$new_remainder};"); // update remainder
 
         	$message = 'Your costs were added!';	
         	sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
         } 
 
-}else {
+}elseif(!empty(is_string($info[0])) && !empty(is_string($info[1])) && !isset($info[2])) {
+	// if user is sending some data to check his costs
+	$name = $info[0];
+	$costs = $info[1];
+	$query = pg_query($link, "SELECT {$costs} FROM {$name} WHERE month = '{$month}';");
+	if($query) {
+	   $result = pg_fetch_result($query, 0, 0);
+	   $message = "You spent $result on $costs this month";
+	   sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
+    }else {
+       $message = 'Try again!';
+       sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
+    }
+
+}else { // if user sent wrong data
 	$message = 'Try again!';	
     sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $message]);
 }
